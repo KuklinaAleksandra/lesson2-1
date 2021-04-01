6e888b3b63d979b395db6bf9abdc2dfebad6f483
Aleksandra <kuklina.aleksandra@rambler.ru>
2021-04-01 22:03:56 +0600

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lesson2_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введите максимальзую температуру воздуха за день  ");
+            int maxT= Convert.ToInt32(Console.ReadLine());
+            Console.Write("Введите минимальную температуру воздуха за день  ");
+            int minT= Convert.ToInt32(Console.ReadLine());
+
+            int mid = (maxT + minT) / 2;
+
+            Console.Write("Среднесуточная температура составила  " + mid);
+        }
+    }
+}
